33ffce182d5ec223679aa8f22b970d50e7c419cf
Ripul MacbookPro <kumar.ripul@gmail.com>
2022-09-05 20:16:49 +0530

    Loggedin Menu and Profile fields translation fix

--- wplms_wpml.php
+++ wplms_wpml.php
@@ -7,17 +7,17 @@ Author: Vibethemes
 Author URI: https://vibethemes.com/
 Text Domain: wplms-wpml
 Domain Path: /languages/
-Version: 1.1
+Version: 1.2
 */
 
 
 
 add_filter('wplms_site_link','wpml_wplms_site_link',10,2);
 function wpml_wplms_site_link($link,$point){
-	if(function_exists('icl_get_home_url')){
-		$link = icl_get_home_url();
-	}
-	return $link;
+  if(function_exists('icl_get_home_url')){
+    $link = icl_get_home_url();
+  }
+  return $link;
 }
 // WPLMS REGISTRATION PAGE CODE
 
@@ -33,9 +33,21 @@ function wplms_wpml_detect_wpml_on_site($registration_link){
 
 
 add_shortcode('wpml_language_switcher',function(){
-	do_action('wpml_add_language_selector');
+  do_action('wpml_add_language_selector');
 });
 
+add_filter('wplms_plugin_instructor_courses','wplms_wpml_courses');
+add_filter('wplms_get_instructor_quizzes','wplms_wpml_courses');
+add_filter('wplms_mycourses','wplms_wpml_courses');
+add_filter('bp_course_wplms_filters','wplms_wpml_courses');
+
+function wplms_wpml_courses($args){
+  global $sitepress;
+ 
+  $current_language = $sitepress->get_current_language();
+  $sitepress->switch_lang($_COOKIE['wp-wpml_current_language'],false);
+  return $args;
+}
 
 add_filter('wplms_course_directory_course_filters',function($args){
 
@@ -128,6 +140,8 @@ function wplms_wpml_update() {
     /* Load Updater Class */
     new WPLMS_Wpml_Auto_Update( $config );
 }
+
+
 add_filter('vibebp_vars',function($data){
   $single_page =vibebp_get_setting('bp_single_page');
   if(function_exists('icl_object_id') && !empty($single_page)){
@@ -136,10 +150,35 @@ add_filter('vibebp_vars',function($data){
   }
   return $data;
 });
+      
 
-add_filter('vibebp_get_setting',function($value,$field){
-  if($field == 'bp_single_page'){
-    $value = icl_object_id($value, 'page', true);
+add_filter('vibebp_bp_xprofile_field',function($f){
+  global $field;
+  $field = $f;
+  if(!empty($_COOKIE['wp-wpml_current_language']))
+  $field['name'] = apply_filters( 'wpml_translate_single_string', $field['name'], 'Buddypress Multilingual','profile field '.$field['id'].' name', $_COOKIE['wp-wpml_current_language']);
+  return $field;
+});
+  
+
+add_action('vibebp_before_notification_loop',function(){
+  if(!empty($_COOKIE['wp-wpml_current_language'])){
+    do_action( 'wpml_switch_language',$_COOKIE['wp-wpml_current_language']);
   }
-  return $value;
-},10,2);
+});
+
+
+add_filter('vibebp_loggedin_menu_nav',function($menu){
+  $menu = 'loggedin';
+  $menuLocations = get_nav_menu_locations(); 
+  $mid = apply_filters('wpml_object_id',$menuLocations[$menu],'nav_menu');
+  $menu = wp_get_nav_menu_items($mid);
+    return $menu;
+});
+
+add_action( 'wpml_enqueued_browser_redirect_language', function($enqueued, $params){
+     if ( ! $enqueued ) {
+      ?><script>sessionStorage.removeItem('loggedinmenu');</script>
+      <?php
+     }
+}, 10, 2 );
